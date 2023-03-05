38a763e8c4d9d338e99f7b52ddc4957bb6ea4de9
Tushar4705 <you@example.com>
2023-03-06 03:18:35 +0530

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Headbar from './navbar.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <Headbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ my-app/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import signin from './Resource/signin.png';
+
+function Home() {
+  return (
+    <div>
+      <img src={signin} alt = "Signin pic"/>
+    </div>
+  )
+}
+
+export default Home
